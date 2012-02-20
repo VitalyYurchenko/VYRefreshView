be28337f718ab8c6f82be18910236eed8a1620fc
Vitaly Yurchenko <Y.Vitaly@mac.com>
2012-02-20 10:43:20 +0200

    Initial Commit

---
+++ RefreshTests/RefreshTests.h
@@ -0,0 +1,13 @@
+//
+//  RefreshTests.h
+//  RefreshTests
+//
+//  Created by Vitaly Yurchenko on 20.02.12.
+//  Copyright (c) 2012 Vitaly Yurchenko. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RefreshTests : SenTestCase
+
+@end
---
+++ RefreshTests/RefreshTests.m
@@ -0,0 +1,32 @@
+//
+//  RefreshTests.m
+//  RefreshTests
+//
+//  Created by Vitaly Yurchenko on 20.02.12.
+//  Copyright (c) 2012 Vitaly Yurchenko. All rights reserved.
+//
+
+#import "RefreshTests.h"
+
+@implementation RefreshTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RefreshTests");
+}
+
+@end
